513c02fecd0006fe4cf515788fa3bb51934f374c
Umme Ruman <85897139+umme786@users.noreply.github.com>
2022-07-31 21:53:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import react, {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const writeSomething = (event) => {
+   setInputList(event.target.value);
+  };
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          To<code>do</code> List
+        </h1>
+
+          <div className="">
+               <input type="text" placeholder="Add Items" style={{padding: "15px", fontSize:"20px", backgroundColor:"lightYellow", border:"none"}} onChange={writeSomething} value={inputList} />
+               <button onClick={listOfItems} style={{padding: "15px", fontSize:"20px", backgroundColor:"skyblue", border:"none"}}> + </button>
+               
+                {Items.map((itemval) => {
+                  return <p> {itemval} </p>
+                })}
+               
+          </div>
+
+        {/* <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href="">
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
